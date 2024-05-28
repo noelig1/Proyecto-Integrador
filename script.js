81b5f378b8a17e5a7b377c3bc4a36ad27ab9a273
noelig1 <166657339+noelig1@users.noreply.github.com>
2024-05-28 19:59:53 -0300

    Proyecto integrador actualizado

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+// Solicita al usuario que introduzca un número entero
+let edad = prompt("Introduce tu edad");
+
+
+let resultado = MayoroMenor(edad);
+
+// Muestra el resultado en una alerta
+alert(" " + edad + "  " + resultado);
+
+// Define la función 
+function MayoroMenor(edad) {
+  if (edad >= 18) {
+    return "Bienvenido!";
+  } else {
+    return "USTED ESTA INGRESANDO BAJO SU RESPONSABILIDAD";
+  }
+}
+*/
